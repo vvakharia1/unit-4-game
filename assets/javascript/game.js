136089f0dd60c0ad1a9e95522d8cb46139db5d51
Vidhi Vaks <vidhi.vakharia93@gmail.com>
2019-09-25 13:17:21 -0400

    added score card and event listeners

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,73 @@
 // 1.0 Creation of buttons on the page for the four crystals
 // look up how to create an image clickable 
 
-var crystalButtons = ["RED", "BLUE", "YELLOW", "GREEN"];
+var crystalButtons = {
+    blue: {
+        name: "Blue",
+        value: 0
+    },
+    green: {
+        name: "Green",
+        value: 0
+    },
+    red: {
+        name: "Red",
+        value: 0
+    },
+    yellow: {
+        name: "Yellow",
+        value: 0,
+    },
+};
 
-function renderButtons() {
-    for (var i = 0; i < crystalButtons.length; i++) {
+// Add value to the scores 
+var currentScore = 0;
+var newScore = 0;
 
-        var crystalBtn = $("<button>"); // creating a variable for the new button and your declaring it as a button 
-        crystalBtn.addClass("color"); // you add the class so that in the css file you can select it (all of them and alter them, all the buttons)
-        crystalBtn.attr({
-            id: crystalButtons[i],
-            value: Math.random() * (99 - 12) + 12 // this is adding two different attr, one is assigning a consistant random number (value) and the other ID attr is assigning an id to each specific crystal
-        }) // this is an html attribute that adds in an id to each crystal when the for loop is run. This helps you differentiate what button was clicked. They will have the id of red or blue etc.. 
-        crystalBtn.text(crystalButtons[i]); // this just add text of the array onto the buttons
-        $("#buttons-view").append(crystalBtn); // this is grabbing the id that has buttons-view and is pinning the buttons onto it
+// Getting randome number
+var randomNumber = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-    }
+// function renderButtons() {
+//     for (var i = 0; i < crystalButtons.length; i++) {
+
+//         var crystalBtn = $("<button>"); // creating a variable for the new button and your declaring it as a button 
+//         crystalBtn.addClass("color"); // you add the class so that in the css file you can select it (all of them and alter them, all the buttons)
+//         crystalBtn.attr({
+//             id: crystalButtons[i],
+//             value: Math.random() * (99 - 12) + 12 // this is adding two different attr, one is assigning a consistant random number (value) and the other ID attr is assigning an id to each specific crystal
+//         }) // this is an html attribute that adds in an id to each crystal when the for loop is run. This helps you differentiate what button was clicked. They will have the id of red or blue etc.. 
+//         crystalBtn.text(crystalButtons[i]); // this just add text of the array onto the buttons
+//         $("#buttons-view").append(crystalBtn); // this is grabbing the id that has buttons-view and is pinning the buttons onto it
+
+//     }
+// }
+// renderButtons();
+
+// Starting the game
+var playGame = function() {
+    currentScore = 0;
+
+    newScore = randomNumber(1, 100); // gets
 }
-renderButtons();
 
-$("#RED").on("click", function(event){
-    console.log(event); //this is an on click function that is grabbing the red button and putting a click on it. once this function runs in the brackets is what you're telling it to do once clicked
+
+// Add value to the buttons once clicked
+
+$(".red-crystal").on("click", function(){
+     addValues(crystalButtons.red)//this is an on click function that is grabbing the red button and putting a click on it. once this function runs in the brackets is what you're telling it to do once clicked
+});
+
+$(".blue-crystal").on("click", function(){
+    addValues(crystalButtons.blue)//this is an on click function that is grabbing the red button and putting a click on it. once this function runs in the brackets is what you're telling it to do once clicked
+});
+
+$(".green-crystal").on("click", function(){
+    addValues(crystalButtons.green)//this is an on click function that is grabbing the red button and putting a click on it. once this function runs in the brackets is what you're telling it to do once clicked
+});
+
+$(".yellow-crystal").on("click", function(){
+    addValues(crystalButtons.yellow)//this is an on click function that is grabbing the red button and putting a click on it. once this function runs in the brackets is what you're telling it to do once clicked
 });
 
